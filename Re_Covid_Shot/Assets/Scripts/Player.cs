c16ab6e3861723973630abc511b9026ec71e4e0a
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-03-16 09:26:38 +0900

    player Move

---
+++ Re_Covid_Shot/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float xMove;
+    float yMove;
+
+    [SerializeField] int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Move();
+
+    }
+
+    void Move()
+    {
+        xMove = Input.GetAxisRaw("Horizontal");
+        yMove = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(new Vector3(xMove, yMove, 0) * speed * Time.deltaTime);
+    }
+}
