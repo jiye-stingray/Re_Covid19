160101ba62fb81fd49fe430894f683c6921abfc0
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-03-16 17:03:25 +0900

    Text 파일 파싱

---
+++ Re_Covid_Shot/Assets/Scripts/Spawn/SpawnData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnData
+{
+    public string type;
+    public int pos;
+    public float delay;
+
+}
---
+++ Re_Covid_Shot/Assets/Scripts/Spawn/SpawnPoints.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SpawnPoints : MonoBehaviour
+{
+
+    [SerializeField] Transform[] spawnPoints;
+
+    [SerializeField] GameObject[] enemies;
+
+    List<SpawnData> spawnList = new List<SpawnData>();
+
+    private void Awake()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPoint("stage1");
+
+        StartCoroutine(SpawnEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnPoint(string stageName)
+    {
+        spawnList.Clear();
+
+        TextAsset textAsset = Resources.Load(stageName) as TextAsset;
+        StringReader stringReader = new StringReader(textAsset.text);
+
+        while (stringReader != null)
+        {
+            string line = stringReader.ReadLine();
+
+            if (line == null)
+                break;
+
+            SpawnData spawnData = new SpawnData();
+
+            spawnData.type = line.Split(',')[0];
+            spawnData.pos = int.Parse(line.Split(',')[1]);
+            spawnData.delay = float.Parse(line.Split(',')[2]);
+
+            spawnList.Add(spawnData);
+
+        }
+
+        stringReader.Close();
+
+
+    }
+
+    GameObject ReturnEnemy(string type)
+    {
+        GameObject enemy = null;
+
+        switch (type)
+        {
+            case "B":
+                enemy = enemies[0];
+                break;
+
+            case "G":
+                enemy = enemies[1];
+                break;
+            case "V":
+                enemy = enemies[2];
+                break;
+            case "C":
+                enemy = enemies[3];
+                break;
+
+            default:
+                break;
+        }
+
+        return enemy;
+    }
+
+
+    IEnumerator SpawnEnemy()
+    {
+        for (int i = 0; i < spawnList.Count; i++)
+        {
+            GameObject enemy = ReturnEnemy(spawnList[i].type);
+            Transform tramsform = spawnPoints[i];
+
+            Instantiate(enemy, tramsform.position, tramsform.rotation);
+
+            yield return new WaitForSeconds(spawnList[i].delay);
+
+        }
+
+    }
+
+}
